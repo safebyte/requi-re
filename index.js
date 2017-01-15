df23e3fd12b1f1cc6eb05ac6d25432eb4b795035
safebyte <service@safebyte.com>
2017-01-15 13:39:55 +0100

    v1.1.1

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ const callerPath = require('caller-path');
 
 module.exports =
 	// determine environment
-	process.env.NODE_ENV == 'production' || !process.env.NODE_ENV
+	process.env.NODE_ENV === 'production' || !process.env.NODE_ENV ?
 		// return default require when in production
-		? require
+		require :
 		// clear require cache in empty env
-		: (moduleId) => {
+		moduleId => {
 			// validate input
-			if(typeof moduleId !== 'string')
+			if (typeof moduleId !== 'string'){
 				throw new TypeError('Expected a string');
+			}
 
 			// resolve path
 			const filePath = resolveFrom(path.dirname(callerPath()), moduleId);
 
 			// delete itself from module parent
-			if(require.cache[filePath] && require.cache[filePath].parent) {
+			if (require.cache[filePath] && require.cache[filePath].parent) {
 				let i = require.cache[filePath].parent.children.length;
 
-				while(i--)
-					if(require.cache[filePath].parent.children[i].id === filePath)
+				while (i--){
+					if(require.cache[filePath].parent.children[i].id === filePath){
 						require.cache[filePath].parent.children.splice(i, 1);
+					}
+				}
 			}
 
 			// delete module from cache
